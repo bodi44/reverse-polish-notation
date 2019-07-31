f0fa2dae911ff3b7be00b409b394cf5a6a21f87a
bodi44 <stogrinec.bodia@gmail.com>
2019-07-31 18:39:23 +0300

    Add html to javascript file

--- src/script/index.js
+++ src/script/index.js
@@ -1,18 +1,49 @@
 const PolishNotationSolver = require('./polishNotationSolver')
 
-const solver = new PolishNotationSolver()
-const input = document.getElementById('equation-input')
-const submit = document.getElementById('equation-submit')
-const output = document.getElementById('output')
+import '../index.css'
 
-submit.addEventListener('click', () => {
-  if (input.value !== '') {
-    output.className = 'success'
-    output.innerHTML = `<h3 class="output-title">Output:</h3>${solver.toPostfixNotation(input.value)}`
-    input.value = ''
-  } else {
-    output.className = 'error'
-    output.innerHTML = `<h3 class="output-title">Error: enter correct equation to convert</h3>`
-    input.value = ''
-  }
+const solver = new PolishNotationSolver()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const converter = document.createElement('div')
+  converter.className = 'converter'
+
+  const title = document.createElement('h1')
+  title.className = 'title'
+  title.innerText = 'Polish Notation Converter'
+
+  const form = document.createElement('div')
+  form.className = 'converter-form'
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.id = 'equation-input'
+
+  const button = document.createElement('button')
+  button.id = 'equation-submit'
+  button.innerText = 'Convert'
+
+  const output = document.createElement('div')
+  output.id = 'output'
+
+  button.addEventListener('click', () => {
+    if (input.value !== '') {
+      output.className = 'success'
+      output.innerHTML = `<h3 class="output-title">Output:</h3>${solver.toPostfixNotation(input.value)}`
+      input.value = ''
+    } else {
+      output.className = 'error'
+      output.innerHTML = `<h3 class="output-title">Error: enter correct equation to convert</h3>`
+      input.value = ''
+    }
+  })
+
+  form.appendChild(input)
+  form.appendChild(button)
+
+  converter.appendChild(title)
+  converter.appendChild(form)
+  converter.appendChild(output)
+
+  document.body.appendChild(converter)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,46 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   context: __dirname + '/src',
 
   entry: './script/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      filename: 'index.html'
+    })
+  ],
 
   watch: false,
 
   module: {
-    rules: [{
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
       }
-    }]
+    ]
   }
 }
